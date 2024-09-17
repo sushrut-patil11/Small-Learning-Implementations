f06b5bdc8321d2ef60b1d527852e7791e8e4efe1
Sush <sushrutpatil5388@outlook.com>
2024-09-17 09:45:02 +0530

    Small changes to user authentication

--- auth-basic/index.js
+++ auth-basic/index.js
@@ -1,108 +1,101 @@
+const express = require("express");
+const jwt = require("jsonwebtoken");
 
-const express = require('express');
-const jwt = require('jsonwebtoken');
-const JWT_SECRET = "Ganapati_Bappa_Morya"
+const JWT_SECRET = "kirat123123";
+
 const app = express();
 app.use(express.json());
 
 const users = [];
 
-function logger(req,res,next){
-    console.log(`${req.method} request came`);
+function logger(req, res, next) {
+    console.log(req.method + " request came");
     next();
 }
 
-app.post("/signup",logger, function (req, res) {
-    const username = req.body.username;
-    const password = req.body.password;
+// localhost:3000
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/public/index.html");
+})
 
+app.post("/signup", logger, function(req, res) {
+    const username = req.body.username
+    const password = req.body.password
     users.push({
         username: username,
         password: password
-    })    
-
-    res.json({
-        message: "You are signed up"
     })
 
-    console.log(users)
-    
+    //checking if a user with this username already exists
+
+    res.json({
+        message: "You are signed in"
+    })
 })
 
-app.post("/signin",logger, function(req, res) {
-    
+app.post("/signin", logger, function(req, res) {
     const username = req.body.username;
     const password = req.body.password;
 
-    // maps and filter
     let foundUser = null;
 
-    for (let i = 0; i<users.length; i++) {
-        if (users[i].username == username && users[i].password == password) {
+    for (let i = 0; i < users.length; i++) {
+        if (users[i].username === username && users[i].password === password) {
             foundUser = users[i]
         }
     }
 
-    if (foundUser) {
+    if (!foundUser) {
+        res.json({
+            message: "Credentials incorrect"
+        })
+        return 
+    } else {
         const token = jwt.sign({
-            username: username,
-            password: password,
-           
-            courses: []
-        }, JWT_SECRET) ;
+            username: foundUser.username
+        }, JWT_SECRET);
+        res.header("jwt", token);
 
-        // foundUser.token = token;
+        res.header("random", "harkirat");
+
         res.json({
             token: token
         })
-    } else {
-        res.status(403).send({
-            message: "Invalid username or password"
-        })
     }
-    console.log(users)
 })
 
+function auth(req, res, next) {
+    const token = req.headers.token;
+    const decodedData = jwt.verify(token, JWT_SECRET);
 
-function auth(req,res,next){
-    const token=req.headers.token;
-    const decodedData=jwt.verify(token,JWT_SECRET);
-    if(decodedData.username){
-        req.username=decodedData.username;
-        next();
-    }else{
+    if (decodedData.username) {
+        // req = {status, headers...., username, password, userFirstName, random; ":123123"}
+        req.username = decodedData.username
+        next()
+    } else {
         res.json({
-            message:"You are not logged in"
+            message: "You are not logged in"
         })
     }
 }
 
-app.get("/me",logger, function(req, res) {
-    let foundUser = null;
+app.get("/me", logger, auth, function(req, res) {
+    // req = {status, headers...., username, password, userFirstName, random; ":123123"}
+    const currentUser = req.username;
+    // const token = req.headers.token;
+    // const decodedData = jwt.verify(token, JWT_SECRET);
+    // const currentUser = decodedData.username
 
     for (let i = 0; i < users.length; i++) {
-        if (users[i].username == req.username)  {
+        if (users[i].username === currentUser) {
             foundUser = users[i]
         }
     }
+
     res.json({
         username: foundUser.username,
         password: foundUser.password
     })
 })
 
-app.get("/todo",logger,auth,function(req,res){
-
-})
-
-
-app.post("/todo",auth,function(req,res){
-
-})
-
-
-app.delete("/todo",auth,function(req,res){
-
-})
-
-app.listen(3000);// that the http server is listening on port 3000
+app.listen(3000);
