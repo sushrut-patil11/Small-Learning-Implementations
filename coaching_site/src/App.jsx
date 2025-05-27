d0117981d828cb8effaf2126ccf1841184eb2c09
Sush <sushrutpatil5388@outlook.com>
2025-05-27 11:25:41 +0530

    Post component Created

--- coaching_site/src/App.jsx
+++ coaching_site/src/App.jsx
@@ -5,22 +5,36 @@ import './App.css'
 
 function App() {
   return(
-    <div>
+    <div style={{background:"#dfe6e9",height:"100vh"}}>
+      <div style={{display:"flex",justifyContent:"center"}}>
       <PostComponent></PostComponent>
-
+      </div>
     </div>
   )
 }
 
-const style={width:200,backgroundColor:"black",borderRadius:10,borderColor:"grey",borderWidth:1}
+const style={width:200,padding:20,backgroundColor:"white",borderRadius:10,borderColor:"grey",borderWidth:1,}
 
 
 function PostComponent(){
-    return <div style={style}>
-      <img src={"https://appx-wsb-gcp-mcdn.akamai.net.in/subject/2023-01-17-0.17044360120951185.jpg"} style={{height:200,width:200,borderRadius:20}}></img>
-    </div>
+    return<div style={style}> 
+      <div style={{display:"flex"}}>
+      
+          <img src={"https://appx-wsb-gcp-mcdn.akamai.net.in/subject/2023-01-17-0.17044360120951185.jpg"} style={{height:30,width:30,borderRadius:20}}></img>
 
+              <div style={{fontSize:10,marginLeft:10}}>
+                  <b>
+                    100xDevs
+                  </b>
+                  <div>28000 Followers</div>
+                  <div>12m</div>
+              </div>
+      </div>
 
+      <div style={{fontSize:12}}>
+          Want to know how to win big??Check out how these guys won $6000 in bounties.
+      </div>
+  </div>
 }
 
 
