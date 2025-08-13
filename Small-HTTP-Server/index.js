ff4fbe5e7a075a46413e8406aca43990aa12d95e
Sush <sushrutpatil5388@outlook.com>
2025-08-13 22:06:28 +0530

    Added some basic routes

--- Small-HTTP-Server/index.js
+++ Small-HTTP-Server/index.js
@@ -1,44 +1,21 @@
-const express =require('express');
-const app=express();
+//creating a small http servers using express
+
+const express=require('express')
+const app=express()
 
 
-app.get("/add",function(req,res){
-   const a=parseInt(req.query.a);
-   const b=parseInt(req.query.b);
+app.get('/',(req,res)=>{
+    res.send('Hello World!')
+})
 
-   res.json({
-    ans:a + b
-   })
-});
-
-app.get("/subtract",function(req,res){
-        const a=parseInt(req.query.a);
-        const b=parseInt(req.query.b);
-     
-    res.json({
-     ans:a-b
-    })
- });
+app.get('/test',(req,res)=>{
+    res.send('Hello from test suite!!')
+})
 
 
-app.get("/multiply",function(req,res){
-    const a=req.query.a;
-    const b=req.query.b;
- 
-    res.json({
-     ans:a*b
-    })
- });
-
-app.get("/divide",function(req,res){
-
-    const a=req.query.a;
-    const b=req.query.b;
- 
-    res.json({
-     ans:a/b
-    })
-});
 
 
-app.listen(3000);
+
+
+
+ app.listen(3000)
